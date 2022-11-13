3ebabb4270df96bf21aaf8171c052f352f928d94
Ananda0909 <maulidiaananda9@gmail.com>
2022-11-13 12:39:36 +0700

    if name == __main__

---
+++ fungsi_tambah.py
@@ -0,0 +1,7 @@
+def tambah(angka_1, angka_2):
+    """Funfsi menambah 2 angka """
+    return angka_1 + angka_2
+
+if __name__ == "__main__":
+    print("Hanya muncul jika dijalankan langsung")
+    print("Tambah : ", tambah(30, 20))
--- program.py
+++ program.py
@@ -1,11 +1,9 @@
-def tambah(angka_1, angka_2):
-    """Funfsi menambah 2 angka """
-    return angka_1 + angka_2
+import fungsi_tambah
 
 print("masukkan angka 1: ")
 angka_1 = float(input())
 print("masukkan angka 2: ")
 angka_2 = float(input())
 
-hasil = tambah(angka_1, angka_2)
+hasil = fungsi_tambah.tambah(angka_1, angka_2)
 print(f"Hasilnya: {hasil}")
